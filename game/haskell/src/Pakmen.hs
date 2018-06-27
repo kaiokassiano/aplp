06e00e778256415c2f19ff7d8e44b4b0e52f3d23
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-06-27 17:47:26 -0300

    Update board to be dynamically generated

--- game/haskell/src/Pakmen.hs
+++ game/haskell/src/Pakmen.hs
@@ -3,7 +3,7 @@ module Pakmen (
   State
 ) where
 
-import UI.HSCurses.Curses hiding (board)
+import UI.HSCurses.Curses hiding (board, move)
 import UI.HSCurses.CursesHelper
 import UI.HSCurses.Logging
 import qualified UI.HSCurses.Widgets
@@ -11,42 +11,29 @@ import System.Exit
 import Control.Monad.Loops
 import Control.Monad
 
-data Cell = Wall | Food | Cherry | Pacman | Ghost | Empty
-type Row = [Cell]
-type Board = [Row]
 type Vector = (Int, Int)
+type Row = [Vector]
+type Board = [Row]
 
-instance Show Cell where
-  show Wall = "#"
-  show Food = "."
-  show Cherry = "C"
-  show Pacman = "U"
-  show Ghost = "G"
-  show Empty = " "
-
-boardHeight = 13
-boardWidth = 9
-
-boardCells = [
-    [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Food, Wall, Wall, Food, Wall, Wall, Food, Wall],
-    [Wall, Food, Food, Wall, Pacman, Wall, Food, Food, Wall],
-    [Wall, Food, Wall, Wall, Food, Wall, Wall, Food, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Food, Food, Food, Food, Food, Food, Food, Wall],
-    [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall]]
+boardHeight = 11
+boardWidth = 7
+pacmanPosition = (3, 4)
+wallPositions = [
+  (1, 3), (2, 3), (4, 3), (5, 3),
+  (2, 4), (4, 4),
+  (1, 5), (2, 5), (4, 5), (5, 5)]
+cherriesPositions = []
 
 data State = State {
-  board :: Board,
   input :: Maybe Char,
   height :: Int,
-  width :: Int
+  width :: Int,
+  move :: Maybe Vector,
+  pacman :: Vector,
+  ghosts :: [Vector],
+  walls :: [Vector],
+  fruits :: [Vector],
+  cherries :: [Vector]
 }
 
 pakmen :: IO State
@@ -58,19 +45,27 @@ pakmen = do
 
 initialState :: IO State
 initialState = return State {
-  board = boardCells,
   input = Nothing,
   height = boardHeight,
-  width = boardWidth
+  width = boardWidth,
+  move = Nothing,
+  pacman = pacmanPosition,
+  ghosts = [],
+  walls = wallPositions,
+  cherries = cherriesPositions,
+  fruits = getAvailableVectors boardWidth boardHeight $ [pacmanPosition] ++ wallPositions ++ cherriesPositions
 }
 
-cell :: Int -> Int -> String
-cell x y = show $ boardCells !! x !! y
+getAvailableVectors :: Int -> Int -> [Vector] -> [Vector]
+getAvailableVectors width height occupied =
+  [(x, y) | x <- [0 .. width - 1], y <- [0 .. height - 1], not $ (x, y) `elem` occupied]
 
 gameOver :: State -> Bool
 gameOver (State {
-    board = boardCurrent
+    pacman = pacmanCurrent,
+    ghosts = ghostsCurrent
   })
+  | pacmanCurrent `elem` ghostsCurrent = True
   | otherwise = False
 
 loopStep :: Window -> State -> IO State
@@ -79,14 +74,23 @@ loopStep win state = do
   getCh >>= \ input ->
     return $ updateState state (inputToTuple input)
 
-inputToTuple :: Key -> Vector
-inputToTuple (KeyChar 'w') = (-1, 0)
-inputToTuple (KeyChar 's') = (1, 0)
-inputToTuple (KeyChar 'a') = (0, -1)
-inputToTuple (KeyChar 'd') = (0, 1)
+inputToTuple :: Key -> Maybe Vector
+inputToTuple (KeyChar 'w') = Just (-1, 0)
+inputToTuple (KeyChar 's') = Just (1, 0)
+inputToTuple (KeyChar 'a') = Just (0, -1)
+inputToTuple (KeyChar 'd') = Just (0, 1)
+inputToTuple _ = Nothing
 
-updateState :: State -> Vector -> State
-updateState state key = state
+updateState :: State -> Maybe Vector -> State
+updateState state move
+  = updatePacman $ updateMove state move
+
+updateMove :: State -> Maybe Vector -> State
+updateMove state inputMove@(Just move) = state { move = inputMove }
+updateMove state _ = state
+
+updatePacman :: State -> State
+updatePacman state@(State { move = (Just vector) }) = state
 
 displayState :: Window -> State -> IO State
 displayState win state = do
@@ -96,6 +100,30 @@ displayState win state = do
   return state
 
 renderBoard :: State -> String
-renderBoard state =
-  unlines $ map (foldl (\acc x -> acc ++ show x) []) (board state)
+renderBoard state
+  = unlines $ applyBorder (width state)
+            $ map (renderRow state)
+            $ blankBoard (width state) (height state)
+
+applyBorder :: Int -> [String] -> [String]
+applyBorder width rows =
+  border ++ map (("#" ++) . (++ "#")) rows ++ border
+  where border = [replicate (width + 2) '#']
+
+renderRow :: State -> [Vector] -> String
+renderRow state =
+  map (characterForPosition state)
+
+characterForPosition :: State -> Vector -> Char
+characterForPosition state position
+  | (pacman state) == position = 'U'
+  | position `elem` walls state = '#'
+  | position `elem` fruits state = '.'
+  | position `elem` ghosts state = 'G'
+  | position `elem` cherries state = 'C'
+  | otherwise = ' '
+
+blankBoard :: Int -> Int -> [[Vector]]
+blankBoard width height =
+  [[(x, y) | x <- [0 .. width - 1]] | y <- [0 .. height - 1]]
 
